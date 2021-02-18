5b7d66212ee1586ddc55e9d013b26d80cde7a66c
Franco Silva <francoder.web@gmail.com>
2021-02-18 10:30:00 -0300

    login jwt

--- src/controllers/auth/cLogin.js
+++ src/controllers/auth/cLogin.js
@@ -1,5 +1,7 @@
 import mLogin from '../../models/mLogin';
 const md5 = require('md5');
+import jwt from 'jsonwebtoken';
+import secret from '../../config/jwt_secret';
 
 class LoginController {
   /**
@@ -12,13 +14,17 @@ class LoginController {
     try {
       const { dni, password } = req.body;
       const user = await mLogin.getUserByDNIandPassword(dni, md5(password));
-      console.log(user);
+
       // Validar codigos de respuesta y chequear error
       if (user.length === 0) {
         res.status(404).send({
           message: `Credenciales no validas`
         });
-      } else res.status(200).send(user);
+      } else {
+        const token = jwt.sign({ sub: user[0].nrodni }, secret, { expiresIn: '2d' });
+
+        res.status(200).send({ user: user[0].nrodni, token });
+      }
     } catch (error) {
       console.log(error);
       res.status(500).send(error);
